f181aef2d42a0ee723c6c87feca67c92473434c8
ethorneloe <eric.thorneloe@daf.qld.gov.au>
2024-09-26 16:10:49 +1000

    Refactor tests

--- ci/pester-tests/post-apply/post-apply.Tests.ps1
+++ ci/pester-tests/post-apply/post-apply.Tests.ps1
@@ -8,72 +8,44 @@ param (
 
 Describe "Test Azure Automation Runbook and Related Resource Creation" {
 
-    # Define the expected resources for each runbook
-    BeforeAll {
+    $runbooks = @(
+        @{
+            Name      = "Test-ExampleRunbook1"
+            Schedules = @("Runbook1-OneTime", "Runbook1-Daily", "Runbook1-Hourly", "Runbook1-Weekly", "Runbook1-Monthly")
+            Variables = @("Runbook1-Environment")
+        },
+        @{
+            Name      = "Test-ExampleRunbook2"
+            Schedules = @("Runbook2-OneTime", "Runbook2-Daily", "Runbook2-Hourly", "Runbook2-Weekly", "Runbook2-Monthly")
+            Variables = @("Runbook2-Secret", "Runbook2-TestVar")
+        },
+        @{
+            Name      = "Test-ExampleRunbook3"
+            Schedules = @("Runbook3-OneTime", "Runbook3-Daily", "Runbook3-Hourly", "Runbook3-Weekly", "Runbook3-Monthly")
+            Variables = @("Runbook3-Environment", "Runbook3-TestVar")
+        }
+    )
 
-        $global:runbooks = @(
-            @{
-                Name      = "Test-ExampleRunbook1"
-                Schedules = @("Runbook1-OneTime", "Runbook1-Daily", "Runbook1-Hourly", "Runbook1-Weekly", "Runbook1-Monthly")
-                Variables = @("Runbook1-Environment")
-            },
-            @{
-                Name      = "Test-ExampleRunbook2"
-                Schedules = @("Runbook2-OneTime", "Runbook2-Daily", "Runbook2-Hourly", "Runbook2-Weekly", "Runbook2-Monthly")
-                Variables = @("Runbook2-Secret", "Runbook2-TestVar")
-            },
-            @{
-                Name      = "Test-ExampleRunbook3"
-                Schedules = @("Runbook3-OneTime", "Runbook3-Daily", "Runbook3-Hourly", "Runbook3-Weekly", "Runbook3-Monthly")
-                Variables = @("Runbook3-Environment", "Runbook3-TestVar")
-            }
-        )
+    It "Should contain the expected automation account resources" -ForEach $runbooks {
+        $runbookName = $_.Name
+        # Check Automation Runbook
+        Write-Host "Testing - $runbookName"
+        $runbookResource = Get-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $runbookName
+        $runbookResource | Should -Not -BeNullOrEmpty
+        $runbookResource.Name | Should -Be $runbookName
 
-        $global:runbookName = $runbook['name']
-    }
+        # Check Automation Schedules
+        foreach ($scheduleName in $_.Schedules) {
+            $schedule = Get-AzAutomationSchedule -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $scheduleName
+            $schedule | Should -Not -BeNullOrEmpty
+            $schedule.Name | Should -Be $scheduleName
+        }
 
-    foreach ($runbook in $runbooks) {
-
-        Context "Runbook $runbookName" {
-            It "Should contain the expected automation account resources" {
-                # Check Automation Runbook
-                $runbookResource = Get-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $runbookName
-                $runbookResource | Should -Not -BeNullOrEmpty
-                $runbookResource.Name | Should -Be $runbookName
-
-                # Check Automation Schedules
-                foreach ($scheduleName in $runbook.Schedules) {
-                    $schedule = Get-AzAutomationSchedule -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $scheduleName
-                    $schedule | Should -Not -BeNullOrEmpty
-                    $schedule.Name | Should -Be $scheduleName
-                }
-
-                # Check Automation Variables
-                foreach ($variableName in $runbook.Variables) {
-                    $variable = Get-AzAutomationVariable -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $variableName
-                    $variable | Should -Not -BeNullOrEmpty
-                    $variable.Name | Should -Be $variableName
-                }
-            }
-
-            It "Should execute the runbook and collect the output" {
-                # Start the Runbook
-                $job = Start-AzAutomationRunbook -AutomationAccountName $AutomationAccountName `
-                    -ResourceGroupName $ResourceGroupName `
-                    -Name $runbookName `
-                    -Parameters @{} -Wait
-
-                # Wait for the job to complete
-                $job | Wait-AzAutomationJob -Timeout 600
-
-                # Get the job output
-                $output = Get-AzAutomationJobOutput -ResourceGroupName $ResourceGroupName `
-                    -AutomationAccountName $AutomationAccountName `
-                    -Id $job.JobId -Stream Output
-
-                # Check the output
-                $output | Should -Not -BeNullOrEmpty
-            }
+        # Check Automation Variables
+        foreach ($variableName in $_.Variables) {
+            $variable = Get-AzAutomationVariable -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $variableName
+            $variable | Should -Not -BeNullOrEmpty
+            $variable.Name | Should -Be $variableName
         }
     }
 }
