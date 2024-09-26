e559b5b1b752b4b58ffa0d83080643c0ee4b2a7a
ethorneloe <eric.thorneloe@daf.qld.gov.au>
2024-09-26 16:48:11 +1000

    Ensure tests align with enabled schedules

--- ci/pester-tests/post-apply/post-apply.Tests.ps1
+++ ci/pester-tests/post-apply/post-apply.Tests.ps1
@@ -11,41 +11,70 @@ Describe "Test Azure Automation Runbook and Related Resource Creation" {
     $runbooks = @(
         @{
             Name      = "Test-ExampleRunbook1"
-            Schedules = @("Runbook1-OneTime", "Runbook1-Daily", "Runbook1-Hourly", "Runbook1-Weekly", "Runbook1-Monthly")
-            Variables = @("Runbook1-Environment")
+            Schedules = @(
+                @{ Name = "Runbook1-OneTime"; Enabled = $true },
+                @{ Name = "Runbook1-Daily"; Enabled = $true },
+                @{ Name = "Runbook1-Hourly"; Enabled = $true },
+                @{ Name = "Runbook1-Weekly"; Enabled = $true },
+                @{ Name = "Runbook1-Monthly"; Enabled = $true }
+            )
+            Variables = @(
+                @{ Name = "Runbook1-Environment"; Encrypted = $false }
+            )
         },
         @{
             Name      = "Test-ExampleRunbook2"
-            Schedules = @("Runbook2-OneTime", "Runbook2-Daily", "Runbook2-Hourly", "Runbook2-Weekly", "Runbook2-Monthly")
-            Variables = @("Runbook2-Secret", "Runbook2-TestVar")
+            Schedules = @(
+                @{ Name = "Runbook2-OneTime"; Enabled = $true },
+                @{ Name = "Runbook2-Daily"; Enabled = $false },
+                @{ Name = "Runbook2-Hourly"; Enabled = $true },
+                @{ Name = "Runbook2-Weekly"; Enabled = $false },
+                @{ Name = "Runbook2-Monthly"; Enabled = $true }
+            )
+            Variables = @(
+                @{ Name = "Runbook2-Secret"; Encrypted = $true },
+                @{ Name = "Runbook2-TestVar"; Encrypted = $false }
+            )
         },
         @{
             Name      = "Test-ExampleRunbook3"
-            Schedules = @("Runbook3-OneTime", "Runbook3-Daily", "Runbook3-Hourly", "Runbook3-Weekly", "Runbook3-Monthly")
-            Variables = @("Runbook3-Environment", "Runbook3-TestVar")
+            Schedules = @(
+                @{ Name = "Runbook3-OneTime"; Enabled = $false },
+                @{ Name = "Runbook3-Daily"; Enabled = $true },
+                @{ Name = "Runbook3-Hourly"; Enabled = $false },
+                @{ Name = "Runbook3-Weekly"; Enabled = $true },
+                @{ Name = "Runbook3-Monthly"; Enabled = $false }
+            )
+            Variables = @(
+                @{ Name = "Runbook3-Environment"; Encrypted = $false },
+                @{ Name = "Runbook3-TestVar"; Encrypted = $false }
+            )
         }
     )
 
     It "Should contain the expected automation account resources" -ForEach $runbooks {
-        $runbookName = $_.Name
+
         # Check Automation Runbook
-        Write-Host "Testing - $runbookName"
+        $runbookName = $_.Name
         $runbookResource = Get-AzAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $runbookName
         $runbookResource | Should -Not -BeNullOrEmpty
         $runbookResource.Name | Should -Be $runbookName
 
         # Check Automation Schedules
-        foreach ($scheduleName in $_.Schedules) {
-            $schedule = Get-AzAutomationSchedule -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $scheduleName
-            $schedule | Should -Not -BeNullOrEmpty
-            $schedule.Name | Should -Be $scheduleName
+        foreach ($schedule in $_.Schedules) {
+            if ($schedule.Enabled) {
+                $scheduleResource = Get-AzAutomationSchedule -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $schedule.Name
+                $scheduleResource | Should -Not -BeNullOrEmpty
+                $scheduleResource.Name | Should -Be $schedule.Name
+            }
         }
 
         # Check Automation Variables
-        foreach ($variableName in $_.Variables) {
-            $variable = Get-AzAutomationVariable -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $variableName
-            $variable | Should -Not -BeNullOrEmpty
-            $variable.Name | Should -Be $variableName
+        foreach ($variable in $_.Variables) {
+            $variableResource = Get-AzAutomationVariable -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name $variable.Name
+            $variableResource | Should -Not -BeNullOrEmpty
+            $variableResource.Name | Should -Be $variable.Name
+            $variableResource.Encrypted | Should -Be $variable.Encrypted
         }
     }
 }
